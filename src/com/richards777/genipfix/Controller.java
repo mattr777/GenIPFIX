6f0cb09bd93688847afff9bbad305f8d127f51a1
Matt Richards <bitbucket@richards777.com>
2015-12-24 09:02:20 -0700

    Initial commit of source code

---
+++ src/com/richards777/genipfix/Controller.java
@@ -0,0 +1,68 @@
+package com.richards777.genipfix;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Controller {
+
+    @FXML
+    private CheckBox checkBox1;
+
+    @FXML
+    private TextField elementName1;
+
+    @FXML
+    private TextField elementSize1;
+
+    @FXML
+    private CheckBox checkBox2;
+
+    @FXML
+    private TextField elementName2;
+
+    @FXML
+    private TextField elementSize2;
+
+    @FXML
+    private CheckBox checkBox3;
+
+    @FXML
+    private TextField elementName3;
+
+    @FXML
+    private TextField elementSize3;
+
+    @FXML
+    private CheckBox checkBox4;
+
+    @FXML
+    private TextField elementName4;
+
+    @FXML
+    private TextField elementSize4;
+
+    @FXML
+    private TextField filename;
+
+    @FXML
+    private Button genButton;
+
+    @FXML
+    private void generateFile(ActionEvent event) {
+        byte data[] = {1, 2, 3, 4};
+        Path file = Paths.get(filename.getText());
+        try {
+            Files.write(file, data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
